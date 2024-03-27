ba0235262d58e593736408c516bae4be3a2a80ef
ManoharNaidu <beesettim27@gmail.com>
2024-03-27 23:40:07 +0530

    Basics -> Variables

---
+++ 1. Basics/1. Variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountName = "Manohar"
+var accountEmail = "manu@gmail.com"
+accountCity = "Vizag"
+let accountState
+
+
+/*
+Do not use var keyword to declare variables. Use let and const instead.
+*/
+
+console.table([accountId, accountName, accountEmail, accountCity, accountState])
+
+accountName = "Naidu"
+accountEmail = "naidu@gmail.com"
+accountCity = "Hyderabad"
+
+console.table([accountId, accountName, accountEmail, accountCity, accountState])
