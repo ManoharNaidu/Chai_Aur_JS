df283be1224725653a4482f75b2d0cdeaba8c8db
Manohar Naidu Bheesetti <33485134+ManoharNaidu@users.noreply.github.com>
2024-06-21 14:28:46 +0000

    comparision

---
+++ 1. Basics/4. Comparision.js
@@ -0,0 +1,13 @@
+console.log("4" < 1);
+
+console.log(null > 0);
+console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+console.log(null < 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+console.log(true === 1);
