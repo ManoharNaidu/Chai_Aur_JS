f7dfc42059ea96e68b985ab01ad4c11bbce25bd9
Manohar Naidu Bheesetti <33485134+ManoharNaidu@users.noreply.github.com>
2024-06-21 14:36:22 +0000

    comparision-2

--- 1. Basics/4. Comparision.js
+++ 1. Basics/4. Comparision.js
@@ -11,3 +11,9 @@ console.log(undefined == 0);
 console.log(undefined >= 0);
 
 console.log(true === 1);
+
+const id = Symbol("id");
+const id2 = Symbol("id");
+
+console.log(id, id2);
+console.log(id == id2);
