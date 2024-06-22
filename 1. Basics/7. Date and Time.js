f8f72f6d16c495d69d0de59be553bf7610f4d8f3
Manohar Naidu Bheesetti <33485134+ManoharNaidu@users.noreply.github.com>
2024-06-22 05:32:58 +0000

    Date & Time

---
+++ 1. Basics/7. Date and Time.js
@@ -0,0 +1,11 @@
+let date = new Date();
+
+console.log(date);
+console.log(date.toString());
+console.log(date.toISOString());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+console.log(date.toUTCString());
