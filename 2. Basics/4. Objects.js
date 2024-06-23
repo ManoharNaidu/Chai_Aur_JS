6a4bc0cf51abe15ae4bd55ddcb65244441897936
Manohar Naidu Bheesetti <33485134+ManoharNaidu@users.noreply.github.com>
2024-06-23 14:29:59 +0000

    objects-2

---
+++ 2. Basics/4. Objects.js
@@ -0,0 +1,17 @@
+const obj = new Object({
+  name: "John",
+  age: 30,
+  email: "beesettim27@gmail.com",
+});
+// console.log(obj);
+
+const User = {
+  email: "example@email.com",
+  userfullname: {
+    firstname: "John",
+    lastname: "Doe",
+  },
+};
+console.log(User);
+
+console.log(Object.keys(User));
