b6a0d472060ccc3db9025be8bfd2d8d3a8c37ffd
Manohar Naidu Bheesetti <33485134+ManoharNaidu@users.noreply.github.com>
2024-06-24 13:06:30 +0000

    scopes

--- 3. Basics/2. Scopes.js
+++ 3. Basics/2. Scopes.js
@@ -11,3 +11,18 @@ if (true) {
 console.log(c); // 3
 
 // the global scope we run in node is different from the global scope in the browser
+
+// ********************* wow ********************* //
+
+console.log(addOne(1)); // 2
+function addOne(num) {
+  return num + 1;
+}
+
+console.log(addTwo(1)); // ReferenceError: addTwo is not defined
+const addTwo = function (num) {
+  return num + 2;
+};
+
+console.log(addThree(1)); // ReferenceError: addThree is not defined
+const addThree = (num) => num + 3;
