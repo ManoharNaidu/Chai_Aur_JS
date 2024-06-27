92654ef04af7b0e8c10207f2e5d21a0617127118
Manohar Naidu Bheesetti <33485134+ManoharNaidu@users.noreply.github.com>
2024-06-27 16:52:38 +0000

    Call stack

--- 3. Basics/5. call stack.js
+++ 3. Basics/5. call stack.js
@@ -1,2 +1,8 @@
 // just for the sake of code pushing cz i want to maintain the streak
 console.log("Urike....fun...hehe");
+
+// JS execution context (EC)
+// it has 2 components
+// 1. Global EC
+// 2. Functional EC
+// 3. Eval EC
