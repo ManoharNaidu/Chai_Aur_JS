e67d967e8b9cf0b75dc6b5fd3858768d0be80abc
Manohar Naidu Bheesetti <33485134+ManoharNaidu@users.noreply.github.com>
2024-06-28 05:16:26 +0000

    ?? & ?:

--- 4. control flow/truthy.js
+++ 4. control flow/truthy.js
@@ -7,3 +7,15 @@ if (manu) {
 }
 
 // truthy is a value that is considered true when encountered in a Boolean context. All values are truthy unless they are defined as falsy (i.e., except for false, 0, -0, 0n, "", null, undefined, and NaN).
+
+// nulish coalescing operator (??): null undefined
+let val1;
+val1 = 5 ?? 10;
+
+console.log(val1);
+
+val1 = null ?? 10;
+
+console.log(val1);
+
+//  Terinary operator (condition ? exprIfTrue : exprIfFalse)
