970dd4d46035ec67dc109d4cf76a3e42086638bf
Manohar Naidu Bheesetti <33485134+ManoharNaidu@users.noreply.github.com>
2024-06-28 05:09:58 +0000

    switch, truthy and falsy

---
+++ 4. control flow/switch.js
@@ -0,0 +1,8 @@
+// switch
+switch (key) {
+  case value:
+    break;
+  default:
+    break;
+  // if break is not included in every case then it will execute all the cases after the matching case except the default case
+}
---
+++ 4. control flow/truthy.js
@@ -0,0 +1,9 @@
+const manu = "m@email.com";
+
+if (manu) {
+  console.log("This is truthy");
+} else {
+  console.log("This is falsy");
+}
+
+// truthy is a value that is considered true when encountered in a Boolean context. All values are truthy unless they are defined as falsy (i.e., except for false, 0, -0, 0n, "", null, undefined, and NaN).
