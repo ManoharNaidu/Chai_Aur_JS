69ec848d187362c3c8966290cd4246dee60a6668
Manohar Naidu Bheesetti <33485134+ManoharNaidu@users.noreply.github.com>
2024-07-05 10:20:04 +0000

    forEach Loop in array

---
+++ 5. iterations/forEach in array.js
@@ -0,0 +1,47 @@
+const coding = ["js", "python", "c++", "java", "ruby"];
+
+// coding.forEach( function(val){
+//     console.log(val);
+// })
+
+// coding.forEach((val) => {
+//   console.log(val);
+// });
+
+// function Print(item) {
+//   console.log(item);
+// }
+
+// // coding.forEach(Print()); // the function is called immediately and the output is undefined
+// coding.forEach(Print); // the function is called correctly
+
+// coding.forEach((item, index, arr) => {
+//   console.log(item, index, arr);
+// });
+
+const MyCoding = [
+  {
+    name: "js",
+    rank: 1,
+  },
+  {
+    name: "python",
+    rank: 2,
+  },
+  {
+    name: "c++",
+    rank: 3,
+  },
+  {
+    name: "java",
+    rank: 4,
+  },
+  {
+    name: "ruby",
+    rank: 5,
+  },
+];
+
+MyCoding.forEach((item) => {
+    
+});
